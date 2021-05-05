df5269833cce4679e6ae6a38ee5f8d22ef30aafd
rafalekqaz <rafaldinho11@gmail.com>
2021-05-05 21:17:07 +0200

    add search in list phrases page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,18 +4,25 @@ import { TableComponent } from './components/table/table.component';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableModule} from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
+import { MatInputModule } from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 @NgModule({
   declarations: [
     TableComponent
   ],
   exports: [
-    TableComponent],
+    TableComponent,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   imports: [
     CommonModule,
     MatPaginatorModule,
     MatTableModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule
   ]
 })
 export class SharedModule { }
